aa316ac1128744ab78bd1827ad8089d3e183f7eb
Rvdixit23 <rvdixit23@gmail.com>
2020-03-25 00:53:53 +0530

    Added options, history retrieval, README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Shell History to Script (Under Development)
+## Objective
+1. This program converts the last few lines of the bash command history into a script
+2. It was inspired by the same feature from MATLAB
+
+## Usage
+```bash
+bash promptToScript.sh [-l <number of lines>] [-c <valid command>] [-f <filename>]
+```
+
+## TODO
+* String Processing
+* Checking for existence of the filename
+* Create the script excluding the lines from this script
+* Adding features for which takes precedence among
+  * Line Number
+  * Command entered
+
+
+
--- promptToScript.sh
+++ promptToScript.sh
@@ -1,11 +1,68 @@
-#!/bin/bash
-HISTFILE=~/.bash_history
-set -o history
-if[[ $1 -eq "-l" ]];then
-    echo "Script based on number of lines"
-    echo "Take file name as argument"
-else if[[ $1 -eq "-c" ]];then
-    echo "Script based on the passed command"
-    echo "Take file name as argument"
+# The standard usage for the script
+usage() { echo "Usage: $0 [-l <number of lines>] [-c <valid command>] [-f <filename>]" 1>&2; exit 1; }
+
+LINES=
+COMMAND=
+
+while getopts ":l:c:f:" o; do
+    case "${o}" in
+        l)
+            LINES=${OPTARG}
+            if ! [[ "$LINES" -eq "$LINES" ]] ; then
+                # Specifying usage if the value is not a number
+                # Numeric value evaluations of non number gives false
+                usage
+            fi
+            # Assigning Line and Command Priorities
+            if [[ -z "${COMMAND}" ]] ; then
+                LINE_PRIO=1
+            else
+                COMMAND_PRIO=1;
+            fi
+            ;;
+        c)
+            COMMAND=${OPTARG}
+            ;;
+        f)
+            FILENAME=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Checking validity of COMMAND
+# @todo Suppress output of which
+if ! [[ -z "${COMMAND}" ]] ; then
+    which ${COMMAND}
+    if [[ $? -eq "1" ]] ; then
+        echo "$0: ${COMMAND}: command not found"
+        usage
     fi
 fi
+
+echo "Number of Lines = ${LINES}"
+echo "Command = ${COMMAND}"
+
+
+# Enable History in a non interactive shell
+HISTFILE=~/.bash_history
+set -o history
+hist=`history|tail -n ${LINES}`
+echo $hist
+
+# @todo
+# String Processing
+# Check if file already exists
+# Create script
+# Exclude Script lines 
+# Command and Line number priorities
+
+# Creating the new script from history
+# echo shabang line and x number of LINES of history to new script
+# echo \#\!\/bin\/bash > $FILENAME.sh; history | tail -n $LINES >> $FILENAME.sh;
+# chmod u+x $FILENAME.sh;
+
+
